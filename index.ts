4fc5024a28e173c7f7e472c1a10a1da4a22625f9
HNocoru <223258@ids.upchiapas.edu.mx>
2024-04-22 15:22:27 -0600

    ez

--- index.ts
+++ index.ts
@@ -8,31 +8,34 @@ async function getEvent() {
     const conn = await amqp.connect(url);
     const channel = await conn.createChannel();
 
-    const exchange = 'amq.topic';
+    const exchange = 'jesusH';
 
-    await channel.assertExchange(exchange, 'topic', {durable: true});
+    await channel.assertExchange(exchange, 'direct', { durable: true });
 
-    const queueName = 'cola';
-    const queue = await channel.assertQueue(queueName, {exclusive: false});
+    const queueName = 'Hernandez';
+    const queue = await channel.assertQueue(queueName, { exclusive: false });
     await channel.bindQueue(queue.queue, exchange, '12345');
 
     console.log('Listening events of RabbitMQ');
 
-    channel.consume(queue.queue, async(mensaje)=>{
-        if(mensaje !== null){
-            console.log(`Message received: ${mensaje.content}`);
+    channel.consume(queue.queue, async (mensaje) => {
+        console.log(mensaje);
+
+        if (mensaje !== null) {
+            const contenido = mensaje.content.toString();
+            console.log(`Contenido recibido: ${contenido}`);
+            const objeto = JSON.parse(contenido);
+            const id = JSON.parse(mensaje.content.toString());
+            const idMmalon = parseInt(id)
+            console.log(`Message received: ${id}`);
             try {
-                const data = JSON.parse(mensaje.content.toString());
-                console.log(data);
-                const Vrms = {tipo:"Vrms", valor: data.Vrms, correo_cliente: "jrmoch2@gmail.com"  }
-                const response = await axios.post('https://back-multi-secundaria.onrender.com/lectura',{tipo:"Vrms", valor: data.Vrms, correo_cliente:"jrmoch2@gmail.com"});
-                const response1 = await axios.post('https://back-multi-secundaria.onrender.com/lectura',{tipo:"Irms", valor: data.Irms, correo_cliente:"jrmoch2@gmail.com"});
-                const response2 = await axios.post('https://back-multi-secundaria.onrender.com/lectura',{tipo:"Power", valor: data.Power, correo_cliente:"jrmoch2@gmail.com"});
-                const response3 = await axios.post('https://back-multi-secundaria.onrender.com/lectura',{tipo:"kWh", valor: data.kWh, correo_cliente:"jrmoch2@gmail.com"});               
+                const response = await axios.post('https://hexagonal-2.onrender.com/registrations', { id_venta: id });
             } catch (error) {
-                console.log("Error sending to API:", error);   
+                console.log(error);
             }
         }
-    }, {noAck:false});
+    }, { noAck: true });
+
 }
-getEvent().catch(console.error);
+getEvent().then((res) => console.log(res)
+).catch(console.error);
